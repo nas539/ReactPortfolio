4075145b1bfa453b7557ac6c53634d4af39f7000
Nicholas Sharp <naas8905@gmail.com>
2020-06-15 13:31:23 -0700

    Changing a couple items

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
     render() {
-        return ( <
-            div className = 'app' >
-            <
-            h1 > Nicholas Sharp Portfolio < /h1>
-
-            <
-            /div>
+        return ( 
+            <div className = 'app' >
+                <h1> Nicholas Sharp Portfolio</h1> 
+                <div>
+                 { moment().format('MMMM Do YYYY, h:mm:ss a') }
+                </div>
+            </div>
         );
     }
 }
