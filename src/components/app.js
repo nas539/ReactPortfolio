08f0c9e1ce2c9cf157fc990c68a398b2c8db3afd
Nicholas Sharp <naas8905@gmail.com>
2020-06-15 14:53:04 -0700

    Adding components in React

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./Portfolio/portfolio-container"
+
 export default class App extends Component {
     render() {
         return ( 
-            <div className = 'app' >
+            <div className = 'app'>
                 <h1> Nicholas Sharp Portfolio</h1> 
+                <PortfolioContainer />
+            
                 <div>
                  { moment().format('MMMM Do YYYY, h:mm:ss a') }
                 </div>
