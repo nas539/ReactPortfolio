0e99df685f5bf93745b57265e6ce6726fabe9a43
Nicholas Sharp <naas8905@gmail.com>
2020-06-18 13:22:00 -0700

    Making it dynamic

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,13 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  
+    
+  
+  
+
   render() {
+   
     return (
       <div className="app">
         <Router>
