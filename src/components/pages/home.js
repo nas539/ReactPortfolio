a953d72fcec959025724f475943cbaa865c3ee19
Nicholas Sharp <naas8905@gmail.com>
2020-06-19 15:50:44 -0700

    Styling buttons

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,7 +4,7 @@ import PortfolioContainer from "../portfolio/portfolio-container";
 export default function() {
     return (
         <div>
-            Homepage
+           
             <PortfolioContainer />
         </div>
     );
