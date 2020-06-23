d759b5a3e88e116461c15cf30fe140d97d002945
Nicholas Sharp <naas8905@gmail.com>
2020-06-23 16:04:11 -0700

    Adding the dropzone and image uploader

--- src/components/Portfolio/portfolio-form.js
+++ src/components/Portfolio/portfolio-form.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import DropzoneComponent from 'react-dropzone-component';
+
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 export default class PortfolioForm extends Component {
     constructor(props) {
@@ -18,6 +22,30 @@ export default class PortfolioForm extends Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.componentConfig = this.componentConfig.bind(this);
+        this.djsConfig = this.djsConfig.bind(this);
+        this.handleThumbDrop = this.handleThumbDrop.bind(this);
+    }
+
+    handleThumbDrop() {
+        return {
+          addedfile: file => this.setState({ thumb_image: file })
+        };
+      }
+
+    componentConfig() {
+        return {
+            iconFiletypes: [".jpeg", ".png"],
+            showFiletypeIcon: true,
+            postUrl: "https://httpbin.org/post"
+        }
+    }
+
+    djsConfig() {
+        return {
+            addRemoveLinks: true,
+            maxFiles: 1
+        }
     }
 
     buildForm() {
@@ -28,6 +56,10 @@ export default class PortfolioForm extends Component {
         formData.append("portfolio_item[url]", this.state.url);
         formData.append("portfolio_item[category]", this.state.category);
         formData.append("portfolio_item[position]", this.state.position);
+
+        if (this.state.thumb_image) {
+            formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
+          }
         return formData
     }
 
@@ -103,6 +135,14 @@ export default class PortfolioForm extends Component {
                         value={this.state.description}
                         onChange={this.handleChange}
                         />
+                   </div>
+
+                   <div className="image-uploaders">
+                       <DropzoneComponent
+                        config={this.componentConfig()}
+                        djsConfig={this.djsConfig()}
+                        eventHandlers={this.handleThumbDrop()}
+                        ></DropzoneComponent>
                    </div>
                    <div>
                        <button type="submit">Save</button>
