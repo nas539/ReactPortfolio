df78a343334770761bdd16518048964c31bc2064
Nicholas Sharp <naas8905@gmail.com>
2020-06-23 13:16:56 -0700

    Adding functions to Portfolio Manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import PortfolioSidebarList from '../Portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../Portfolio/portfolio-form';
+
 
 export default class PortfolioManger extends Component {
-    render() {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioiItems: []
+    }
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  } 
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+
+  }
+  
+  handleFormSubmissionError(error) {
+    console.log("Error", error)
+  }
+  getPortfolioItems() {
+    axios.get("https://nicholasasharp.devcamp.space/portfolio/portfolio_items", { 
+      withCredentials: true
+    }).then(response => {
+      this.setState({
+        portfolioiItems: [...response.data.portfolio_items]
+      })
+    }).catch(error => {
+      console.log("error in getting portfolio item", error)
+    })
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
         return (
             <div className="portfolio-manager-wrapper">
             <div className="left-column">
-              <h1>Portfolio form....</h1>
+              <PortfolioForm />
             </div>
           
             <div className="right-column">
-              <h1>Portfolio sidebar....</h1>
+              <PortfolioSidebarList 
+                data={this.state.portfolioiItems} />
             </div>
           </div>
         )
