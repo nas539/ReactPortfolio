0396066427bb3e120455f6379d01f8a7f31f4570
Nicholas Sharp <naas8905@gmail.com>
2020-06-25 12:53:32 -0700

    Finishing the portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -14,7 +14,8 @@ export default class PortfolioManger extends Component {
       portfolioToEdit: {}
     }
 
-    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+    this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
     this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
     this.handleDeleteClick =this.handleDeleteClick.bind(this);
     this.handleEditClick = this.handleEditClick.bind(this);
@@ -51,7 +52,11 @@ export default class PortfolioManger extends Component {
   });
   }
 
-  handleSuccessfulFormSubmission(portfolioItem) {
+  handleEditFormSubmission() {
+    this.getPortfolioItems();
+  }
+
+  handleNewFormSubmission(portfolioItem) {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
     });
@@ -82,7 +87,8 @@ export default class PortfolioManger extends Component {
             <div className="portfolio-manager-wrapper">
             <div className="left-column">
               <PortfolioForm 
-                handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                handleNewFormSubmission={this.handleNewFormSubmission}
+                handleEditFormSubmission={this.handleEditFormSubmission}
                 handleFormSubmissionError={this.handleFormSubmissionError}
                 clearPortfolioToEdit={this.clearPortfolioToEdit}
                 portfolioToEdit={this.state.portfolioToEdit}
