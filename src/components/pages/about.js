529afe3a7a2930a68743299f4e697996c2f05bb2
Nicholas Sharp <naas8905@gmail.com>
2020-06-29 14:53:36 -0700

    Adding the contact and about page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import profilePicture from "../../../static/assets/bio/IMG_0699.jpg"
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div 
+            className="left-column"
+            style={{
+                background: "url(" + profilePicture + ") no-repeat",
+                backgroundSize: "cover",
+                backgroundPosition: "center"
+ 
+            }}
+            />
+            <div className="right-column">
+            Enthusiastic software engineer, that is always eager to learn and use new technologies. 
+            My passion is solving problems, either through communication or through code.
+            Creative by nature and detail-oriented with an entrepreneurial spirit.
+            With several years of customer service and management experience, I bring a focus on getting the job done. 
+            When I am not coding I am eating good food, spending time with my family, hiking trails, or playing with my dog.
+            </div>
         </div>
     );
 }
